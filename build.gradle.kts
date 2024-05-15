af9869e8a09e6141a5decca1ee7480c6ff243899
Maggie <117408510+Gamt0n@users.noreply.github.com>
2024-05-14 21:03:21 -0400

    hello world

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,8 @@ repositories {
 }
 
 dependencies {
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("org.slf4j:slf4j-simple:2.0.10")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
--- src/main/java/dev/twelveoclock/recipesite/Main.java
+++ src/main/java/dev/twelveoclock/recipesite/Main.java
@@ -1,7 +1,14 @@
 package dev.twelveoclock.recipesite;
+import io.javalin.Javalin;
+import io.javalin.config.JavalinConfig;
+import java.util.function.Consumer;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		Javalin app = Javalin.create(javalinConfig -> {
+			javalinConfig.staticFiles.add("public");
+		})
+			//.get("/", ctx -> ctx.result("Hello World"))
+			.start(7070);
 	}
 }
